b662b011b59e59068402c042beab3619f05d2871
Frank Grundin <frank.grundin@nordicmedtest.se>
2018-08-29 16:19:15 +0200

    chmod

--- soaptest/testrunner_all.sh
+++ soaptest/testrunner_all.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+# -j: Output jUnit XML report
+# -r: Pretty print the result at the end of the run.
 find . -maxdepth 1 -iname '*soapui-project.xml' -exec echo {} \; -exec /opt/soapui/bin/testrunner.sh {} -j -r \;
