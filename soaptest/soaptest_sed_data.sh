57b78c8250d51727351230ba9f0fe05afed955a9
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2023-10-23 14:53:48 +0200

    Lagt till qabks i sed-script

--- soaptest/soaptest_sed_data.sh
+++ soaptest/soaptest_sed_data.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters"
-    echo "./soaptest_sed_data.sh {dev|test|qa}"
+    echo "./soaptest_sed_data.sh {dev|test|qa|qabks}"
     exit
 fi
 
@@ -13,20 +13,25 @@ elif [ "$1" = "test" ]; then
     COOPERATION_URL="https://test.api.ntjp.se"
     # COOPERATION_URL="http://ind-ttjp-apache-api-vip.ind1.sth.basefarm.net"
 elif [ "$1" = "qa" ]; then
-    COOPERATION_URL="https://stage.api.ntjp.se"
-    # COOPERATION_URL="http://ind-stjp-apache-api-vip.ind1.sth.basefarm.net"
+    COOPERATION_URL="http://ind-stjp-apache-api-vip.ind1.sth.basefarm.net"
+elif [ "$1" = "qabks" ]; then
+    COOPERATION_URL="https://stage.ntjp.se"
 else
     echo "Illegal parameter"
-    echo "./soaptest_sed_data.sh {dev|test|qa}"
+    echo "./soaptest_sed_data.sh {dev|test|qa|qabks}"
     exit
 fi
-TARGETHOST="$1.esb.ntjp.se"
+
+if [ "$1" = "qabks" ]; then
+    TARGETHOST="esb.stage.ntjp.se"
+else
+    TARGETHOST="$1.esb.ntjp.se"
+fi
+
 SOURCESYSTEMHSA="LOAD-MOCKS"
-# COOP_AUTH_HEADER="Basic base64EncodedusernamePass"
 
 echo "Tests will be run against the following host: ${TARGETHOST}"
 
 sed -e 's@SOURCESYSTEMHSA@'"${SOURCESYSTEMHSA}"'@; s@TARGETHOST@'"${TARGETHOST}"'@' \
-    -e "s,COOPERATION,$COOPERATION_URL,"\
-    # -e 's@COOPAUTH@'"${COOP_AUTH_HEADER}"'@' \
+    -e  "s,COOPERATION,$COOPERATION_URL,"\
         data-sed.xml > data.xml
